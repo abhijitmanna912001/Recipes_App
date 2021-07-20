e3f820bee62d23860764039cbddfb33b1c4b019c
Abhijit Manna <abhijitmanna912001@gmail.com>
2021-07-20 22:33:56 +0530

    project completed

---
+++ src/components/RandomMeal.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+
+import Meal from "./Meal";
+
+const API_URL = " https://www.themealdb.com/api/json/v1/1/random.php";
+
+const RandomMeal = () => {
+  const [meal, setMeal] = useState(undefined);
+
+  useEffect(() => {
+    async function getMeal() {
+      const res = await fetch(API_URL);
+      const data = await res.json();
+
+      setMeal(data.meals[0]);
+    }
+    getMeal();
+  }, []);
+  if (!meal) return null;
+
+  return (
+    <section className="container">
+      <h2>Featured Meal</h2>
+      <Meal meal={meal} />
+    </section>
+  );
+};
+
+export default RandomMeal;
